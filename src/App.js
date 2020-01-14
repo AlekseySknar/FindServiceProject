efc8f9cc177d28337247e2fd9f01292b7c989ca3
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-01-14 23:00:15 +0700

    New Eba
    add new yoba

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
 import SimpleContainer from "./GetServiseForms";
 
-
-
-export default  class App extends Component {
+export default class App extends Component {
   render() {
     return (
-        <div className="App">
-          <h1>Hello, eba!</h1>
-            <SimpleContainer/>
-        </div>
+      <div className="App">
+        <h1>Hello, eba!</h1>
+        <h1>Hello eba 2</h1>
+        <SimpleContainer />
+      </div>
     );
   }
 }
-
-
